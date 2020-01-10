509d99f5c632ca90ba65435382a1d23dc82170a1
juddinreonomy <juddin@reonomy.com>
2020-01-10 16:40:50 -0500

    fix time zone.

--- app.py
+++ app.py
@@ -109,16 +109,6 @@ def status():
     return {"Message": "ok"}, 200
 
 
-@app.route('/')
-def root():
-    return render_template("index.html")
-
-
-@app.route('/index')
-def index():
-    return render_template("index.html")
-
-
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     return render_template('%s.html' % page_name)
